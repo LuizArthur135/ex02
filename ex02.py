6a2b7984224943a08237acd8115c7fff9d914c66
Luiz Arthur <96485635+LuizArthur135@users.noreply.github.com>
2022-01-01 13:19:14 -0300

    ex02

---
+++ ex02.py
@@ -0,0 +1,8 @@
+lista = ['M', 'u', 'me', 'Ke']
+lista1 = ['e', 'no', 'é', 'lly']
+frase = []
+
+for cont in range(0, len(lista)):
+    frase.append(lista[cont])
+    frase.append(lista1[cont])
+print(frase)
